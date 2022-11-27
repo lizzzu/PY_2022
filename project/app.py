4f889eb2f56ea11b4a612ccb445c7a454641ce09
lizzzu <lizudim@yahoo.com>
2022-11-28 00:59:18 +0200

    some refactoring

--- project/app.py
+++ project/app.py
@@ -1,5 +1,5 @@
-import json
 from flask import Flask
+from pysondb import db as DB
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
@@ -9,55 +9,44 @@ app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 @app.route('/countries/')
 @app.route('/countries/all/')
 def all_countries():
-    db = json.load(open('db.json', 'r', encoding='utf-8'))
-    return db['data']
+    return DB.getDb('db.json').getAll()
 
-@app.route('/countries/<key>/')
-def countries_key(key):
-    db = json.load(open('db.json', 'r', encoding='utf-8'))
+@app.route('/countries/by/<key>/')
+def countries_by_key(key):
+    db = DB.getDb('db.json').getAll()
+    key = key.lower().replace('-', ' ')
+    if key not in db[0].keys():
+        return "ERROR: wrong key"
+    return [{key: j[key]} for j in db]
+
+@app.route('/countries/by/<key>/top/')
+@app.route('/countries/by/<key>/top/<k>/')
+def top_by_key(key, k='207'):
+    if key not in ['area', 'population', 'density']:
+        return "ERROR: wrong key"
+    db = [{'name': j['name'], key: j[key]} for j in DB.getDb('db.json').getAll()]
+    return sorted(db, key=lambda d: d[key], reverse=True)[:int(k)]
+
+@app.route('/countries/by/<key>/<value>')
+def countries_by_key_value(key, value):
+    db = DB.getDb('db.json').getAll()
     key = key.replace('-', ' ')
-    db['data'] = [{key: j[key]} for j in db['data']]
-    return db['data']
-
-@app.route('/countries/area/top/')
-@app.route('/countries/area/top/<k>/')
-def top_by_area(k='207'):
-    db = json.load(open('db.json', 'r', encoding='utf-8'))
-    db['data'] = [{'name': j['name'], 'area': j['area']} for j in db['data']]
-    return sorted(db['data'], key=lambda d: d['area'], reverse=True)[:int(k)]
-
-@app.route('/countries/population/top/')
-@app.route('/countries/population/top/<k>/')
-def top_by_population(k='207'):
-    db = json.load(open('db.json', 'r', encoding='utf-8'))
-    db['data'] = [{'name': j['name'], 'population': j['population']} for j in db['data']]
-    return sorted(db['data'], key=lambda d: d['population'], reverse=True)[:int(k)]
-
-@app.route('/countries/density/top/')
-@app.route('/countries/density/top/<k>')
-def top_by_density(k='207'):
-    db = json.load(open('db.json', 'r', encoding='utf-8'))
-    db['data'] = [{'name': j['name'], 'density': j['density']} for j in db['data']]
-    return sorted(db['data'], key=lambda d: d['density'], reverse=True)[:int(k)]
-
-@app.route('/countries/language/<language>')
-def country_language(language):
-    db = json.load(open('db.json', 'r', encoding='utf-8'))
-    language = language.replace('-', ' ')
-    return [j for j in db['data'] if any(l for l in j['languages'].split(';') if language in l.lower())]
-
-@app.route('/countries/religion/<religion>')
-def country_religion(religion):
-    db = json.load(open('db.json', 'r', encoding='utf-8'))
-    religion = religion.replace('-', ' ')
-    return [j for j in db['data'] if any(r for r in j['religion'].split(';') if religion in r.lower())]
-
-@app.route('/countries/government/<government>')
-def country_government(government):
-    db = json.load(open('db.json', 'r', encoding='utf-8'))
-    return [j for j in db['data'] if government in j['government'].lower()]
-
-@app.route('/countries/time-zone/<time_zone>')
-def country_time_zone(time_zone):
-    db = json.load(open('db.json', 'r', encoding='utf-8'))
-    return [{'name': j['name'], 'time zone': j['time zone']} for j in db['data'] if time_zone == j['time zone'].lower()]
+    value = value.lower()
+    if key == 'name':
+        value = value.replace('-', '_').replace(' ', '_')
+        return [{key: j[key]} for j in db if value == j[key].lower()]
+    elif key in ['capital', 'largest city', 'driving side']:
+        value = value.replace('+', ' ')
+        return [{'name': j['name'], key: j[key]} for j in db if value == j[key].lower()]
+    elif key in ['language', 'religion']:
+        value = value.replace('-', ' ').replace('+', ' ')
+        return [{'name': j['name'], key: j[key]} for j in db if any(v for v in j[key].split(';') if value in v.lower())]
+    elif key == 'government':
+        value = value.replace('+', ' ')
+        return [{'name': j['name'], key: j[key]} for j in db if value in j[key].lower()]
+    elif key in ['area', 'population', 'density']:
+        return [{'name': j['name'], key: j[key]} for j in db if value == str(j[key])]
+    elif key in ['time zone', 'currency', 'calling code']:
+        return [{'name': j['name'], key: j[key]} for j in db if value == j[key].lower()]
+    else:
+        return "ERROR: wrong key"
