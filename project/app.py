4a2f8b21f61608fe4e7306754cd4d857e015fd72
lizzzu <lizudim@yahoo.com>
2022-12-06 17:45:34 +0200

    huge refactoring

--- project/app.py
+++ project/app.py
@@ -7,8 +7,15 @@ app.config['JSON_SORT_KEYS'] = False
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 def bad_request_page():
-    return '<head><title>400 Bad Request</title></head>\
-        <body><h1 style="margin-top: 1.5rem">Bad Request</h1><p>Wrong key</p></body>'
+    return """
+        <head>
+            <title>400 Bad Request</title>
+        </head>
+        <body>
+            <h1 style="margin-top: 1.5rem">Bad Request</h1>
+            <p>Wrong key</p>
+        </body>
+    """
 
 @app.route('/')
 @app.route('/countries/')
@@ -18,11 +25,7 @@ def all_countries():
     Returns an array of JSON objects, each one corresponding to
     a specific country and containing all the data found about it.
     """
-    db = DB.getDb('db.json').getAll()
-    for j in db:
-        for key in j.keys():
-            j[key] = j[key].split(';') if key in ['languages', 'religion'] else j[key]
-    return db
+    return DB.getDb('db.json').getAll()
 
 @app.route('/countries/by/<string:key>/')
 def countries_by_key(key):
@@ -38,8 +41,6 @@ def countries_by_key(key):
         return Response(bad_request_page(), status=400)
     if key == 'name':
         return [{key: j[key]} for j in db]
-    if key in ['languages', 'religion']:
-        return [{'name': j['name'], key: j[key].split(';')} for j in db]
     return [{'name': j['name'], key: j[key]} for j in db]
 
 @app.route('/countries/by/<string:key>/top/')
@@ -63,18 +64,12 @@ def countries_by_key_value(key, value):
     Returns an array of JSON objects containing the name and the pair
     `(key, value)` for each country that matches the given pair.
     """
-    db = DB.getDb('db.json').getAll()
+    db = DB.getDb('db.json')
     key = key.replace('+', ' ').replace('-', ' ')
-    if key in ['name', 'capital', 'largest city', 'languages', 'religion', 'government']:
-        value = value.lower().replace('+', ' ')
-        if key == 'name':
-            return [{key: j[key]} for j in db if value == j[key].lower()]
-        if key in ['capital', 'largest city']:
-            return [{'name': j['name'], key: j[key]} for j in db if value == j[key].lower()]
-        if key in ['languages', 'religion']:
-            return [{'name': j['name'], key: j[key].split(';')} for j in db if any(v for v in j[key].split(';') if value in v.lower())]
-        if key == 'government':
-            return [{'name': j['name'], key: j[key]} for j in db if value in j[key].lower()]
-    if key in ['area', 'population', 'density', 'driving side', 'currency', 'time zone', 'calling code', 'id']:
-        return [{'name': j['name'], key: j[key]} for j in db if value.lower() == str(j[key]).lower()]
+    if key == 'name':
+        return [{key: j[key]} for j in db.reSearch(key, f'(?i).*{value.replace("+", " ")}')]
+    elif key in ['capital', 'largest city', 'languages', 'religions', 'government']:
+        return [{'name': j['name'], key: j[key]} for j in db.reSearch(key, f'(?i).*{value.replace("+", " ")}')]
+    elif key != 'link':
+        return [{'name': j['name'], key: j[key]} for j in db.getBy({key: value.upper()})]
     return Response(bad_request_page(), status=400)
--- project/wiki.py
+++ project/wiki.py
@@ -1,6 +1,6 @@
-import re
 import time
 import requests
+from re import match, search
 from pysondb import db as DB
 from bs4 import BeautifulSoup
 
@@ -8,10 +8,10 @@ start_time = time.time()
 
 def get_info(country, link):
     """
-    Builds and returns a dictionary for each country
-    by crawling the wikipedia page at the received url.
+    Builds and returns a dictionary for each country by crawling 
+    the wikipedia page at the received url.
     """
-    country = ' '.join([c for _, c in reversed(list(enumerate(country.replace(',_', ',').split(','))))]).replace('_', ' ')
+    country = ' '.join([c for c in country.replace(',_', ',').split(',')[::-1]]).replace('_', ' ')
     m, s = divmod(int(time.time() - start_time), 60)
     print(f'[{m:02d}:{s:02d}] {country}')
     info = {
@@ -19,8 +19,8 @@ def get_info(country, link):
         'link': link,
         'capital': '',
         'largest city': '',
-        'languages': '',
-        'religion': '',
+        'languages': [],
+        'religions': [],
         'government': '',
         'area': -1.0,
         'population': -1,
@@ -35,47 +35,55 @@ def get_info(country, link):
         th = row.find('th', attrs={'class': 'infobox-label'})
         td = row.find('td', attrs={'class': 'infobox-data'})
         if th or td:
-            title = re.search(r'\w+([.\[\]\s]*\w+)*', th.text)
+            title = search(r'\w+([.\[\]\s]*\w+)*', th.text)
             if title:
                 title = title.group()
                 if any(s in title for s in ['Capital', 'capital']):
-                    data = td.find_all('a')
-                    info['capital'] = data[1].text if data[0].text == 'de jure' else data[0].text
+                    data = [d.text for d in td.find_all('a')]
+                    info['capital'] = data[1] if data[0] == 'de jure' else data[0]
                 if any(s in title for s in ['Largest city', 'largest city']):
                     info['largest city'] = td.find('a').text
-                if any(s in title for s in ['Official', 'National language', 'Major language', 'Working language']):
-                    languages = [d.text for d in td.find_all('a') if not re.search(r'[\d\[]', d.text) and d.text[0].isupper()]
-                    if info['languages'] == '': info['languages'] = ';'.join(languages)
-                    else: info['languages'] += ';' + ';'.join(languages)
+                if any(s in title for s in ['Official', 'National lang', 'Major lang', 'Working lang']):
+                    data = [d.text for d in td.find_all('a')]
+                    lang = [d for d in data if not search(r'[\d\[]', d) and d[0].isupper()]
+                    if not data:
+                        lang = [d.text for d in td.find_all('li')]
+                        if lang == []: lang = [td.text]
+                    info['languages'] += lang
                 if 'Religion' in title:
-                    info['religion'] = ';'.join([d.text for d in td.find_all('a') if not re.search(r'[\d\[]', d.text) and d.text.lower() not in ['official', 'see below']])
+                    data = [d.text for d in td.find_all('a')]
+                    relg = [d for d in data if not search(r'[\d\[]', d) and d.lower() not in ['official', 'see below']]
+                    info['religions'] += relg
                 if 'Government' in title:
-                    info['government'] = ''.join([char for char in td.text.replace('−', '-').replace('–', '-') if not char.isdigit() and char not in ['[', ']']])
+                    data = td.text.replace('−', '-').replace('–', '-').replace(' ', ' ')
+                    govt = [d for d in data if not d.isdigit() and d not in ['[', ']']]
+                    info['government'] = ''.join(govt)
                 if any(s in title for s in ['Total', 'Including', 'Land']) and 'km2' in td.text:
-                    info['area'] = float(re.match(r'(\d+[.,]?)+', td.text).group().replace(',', ''))
+                    info['area'] = float(match(r'(\d+[.,]?)+', td.text).group().replace(',', ''))
                 if any(s in title for s in ['estimate', 'census']) and info['population'] < 0:
-                    info['population'] = int(re.search(r'(\d+[., ]?)+', td.text).group().replace(' ', '').replace(',', ''))
+                    info['population'] = int(search(r'(\d+[., ]?)+', td.text).group().replace(' ', '').replace(',', ''))
                     info['density'] = float('%.1f' % (info['population'] / info['area']))
                 if 'Driving' in title:
-                    info['driving side'] = re.match(r'\w+', td.text).group()
+                    info['driving side'] = match(r'\w+', td.text).group()
                 if 'Currency' in title:
-                    currency = re.search(r'\(([A-Z]{2,3}|¥)', td.text)
-                    if currency: info['currency'] = currency.group()[1:]
-                    else: info['currency'] = re.search(r'[A-Z][a-z]+( [a-z]+)', td.text).group()
+                    curr = search(r'\(([A-Z]{2,3}|¥)', td.text)
+                    if curr: info['currency'] = curr.group()[1:]
+                    else: info['currency'] = search(r'[A-Z][a-z]+( [a-z]+)', td.text).group()
                 if 'Time zone' in title:
-                    time_zone = re.search(r'UTC([+-−]\d{1,2})?', td.text).group().replace('−', '-').replace('–', '-')
-                    if len(time_zone) == 6 and time_zone[-2] == '0': time_zone = time_zone[:-2] + time_zone[-1]
-                    info['time zone'] = time_zone
+                    tz = search(r'UTC([+-−]\d{1,2})?', td.text).group().replace('−', '-').replace('–', '-')
+                    if len(tz) == 6 and tz[-2] == '0': tz = tz[:-2] + tz[-1]
+                    if len(tz) == 5 and tz[-1] == '0': tz = tz[:-2]
+                    info['time zone'] = tz
                 if 'Calling code' in title:
-                    code = re.match(r'\+\d+', td.text)
+                    code = match(r'\+\d+', td.text)
                     if code: info['calling code'] = code.group()
                     else: info['calling code'] = td.find('li').find('a').text
     return info
 
 def init_db():
     """
-    Initializes the JSON database, iterating through
-    all the countries and adding the corresponding info.
+    Initializes the JSON database, iterating through all countries 
+    and adding the corresponding info.
     """
     root = 'https://en.wikipedia.org'
     html = BeautifulSoup(requests.get(f'{root}/wiki/List_of_sovereign_states').content, 'html.parser')
